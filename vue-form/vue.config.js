4f8cb9dd5f62cb130948e912248fb78b5e350f3e
NewBean0312 <newbean0312@gmail.com>
2024-07-04 15:46:39 +0900

    axios를 이용한 데이터 전송 및 form 구현

--- vue-form/vue.config.js
+++ vue-form/vue.config.js
@@ -2,8 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   runtimeCompiler: true,
-
-  devServer: {
-    overlay: false,
-  },
 });
